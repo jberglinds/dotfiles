be25a15ac875fa28baf003bed29725a4162dff6c
Jonathan Berglind <jonathan.berglind@gmail.com>
2016-10-17 17:20:37 +0200

    Atom packages added to install script

--- atom/install.sh
+++ atom/install.sh
@@ -2,3 +2,9 @@
 
 # symlink stuff
 ln -isv "$HOME/dotfiles/atom/config.cson" "$HOME/.atom/config.cson"
+
+# install packages
+apm install --packages-file packages.txt
+
+# backup packages with:
+# apm list --installed --bare | grep '^[^@]\+' -o
