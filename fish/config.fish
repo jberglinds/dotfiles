9fc39d4b6e9242c6cd07c9ab9b4ce3c09fce753f
Jonathan Berglind <jonathan.berglind@gmail.com>
2017-10-24 14:40:30 +0200

    Reindent stuff, small updates Add vimconf command, update fasd

--- fish/config.fish
+++ fish/config.fish
@@ -1,11 +1,3 @@
-# path to Oh My Fish install.
-set -q XDG_DATA_HOME
-  and set -gx OMF_PATH "$XDG_DATA_HOME/omf"
-  or set -gx OMF_PATH "$HOME/.local/share/omf"
-
-# load Oh My Fish configuration.
-source $OMF_PATH/init.fish
-
 # remove annoying welcome message
 set fish_greeting ""
 
@@ -64,27 +56,40 @@ function z
 	set -l dir (fasd -Rdl $argv[1] | fzf -1 -0 --no-sort +m); and cd $dir; return 1
 end
 
-# moving around folders
-function -
-  cd -
+# kill ports easily
+function killport
+	if set -q argv[1]
+		kill -9 (lsof -t -i:$argv[1])
+end
 end
 
 function ...
-  cd ../.. $argv;
+	cd ../.. $argv;
 end
 
 function ....
-  cd ../../.. $argv;
+	cd ../../.. $argv;
 end
 
 # saving my ass
 alias cp='cp -i'
 alias mv='mv -i'
 
+# thefuck
+thefuck --alias | source
+
 # git stuff
 alias git='hub'
 
+# nicer grep
 alias grep='grep --color=auto'
 
+# easily show and hide hidden files in Finder
 alias showFiles='defaults write com.apple.finder AppleShowAllFiles YES; killall Finder /System/Library/CoreServices/Finder.app'
 alias hideFiles='defaults write com.apple.finder AppleShowAllFiles NO; killall Finder /System/Library/CoreServices/Finder.app'
+
+# edit vimrc easily
+alias vimconf='vim ~/.vim/vimrc'
+
+# iTerm2 shell integration
+test -e {$HOME}/.iterm2_shell_integration.fish ; and source {$HOME}/.iterm2_shell_integration.fish
