0a1306ff6837e9557b36e8031ef01e069ade8ea5
Jonathan Berglind <jonathan.berglind@gmail.com>
2018-03-28 12:41:04 +0200

    Try new pretty format for git log

--- fish/abbr.fish
+++ fish/abbr.fish
@@ -2,7 +2,7 @@ abbr g git
 abbr ga 'git add'
 abbr gc 'git commit'
 abbr gd 'git diff'
-abbr gl 'git log --format=babe'
+abbr gl 'git log --format=compact'
 abbr gp 'git pull --rebase'
 abbr gs 'git status -s'
 abbr webserver 'python3 -m http.server'
