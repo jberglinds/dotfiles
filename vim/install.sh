2a3d0e79d4ad2fb58efbc5bce67177cd451a3dee
Jonathan Berglind <jonathan.berglind@gmail.com>
2018-07-18 12:05:18 +0200

    vim -> neovim

--- vim/install.sh
+++ vim/install.sh
@@ -1,13 +1,15 @@
 #!/usr/bin/env fish
-
-mkdir ~/.vim/
+mkdir -p ~/.vim ~/.nvim ~/.config/nvim
 
 # install vim-plug
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
   https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
-# symlink vimrc
+# symlink dotfiles vimrc to .vim/vimrc
 ln -isv ~/dotfiles/vim/vimrc ~/.vim/vimrc
+# symlink vim stuff to neovim
+ln -isv ~/.vim/vimrc ~/.config/nvim/init.vim
+ln -isv ~/.vim/autoload ~/.nvim/autoload
 
 # install plugins
 vim -c ':PlugInstall' -c 'qa!'
