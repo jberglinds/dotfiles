0e520085f674595102678a21db07b4f4823c6b4c
Jonathan Berglind <jonathan@Jonathans-MacBook-Pro.local>
2019-07-13 14:33:34 +0200

    Update brewfile

--- homebrew/install.sh
+++ homebrew/install.sh
@@ -1,10 +1,7 @@
 #!/usr/bin/env bash
 
-# set permissions something something
-sudo chown -R $(whoami):admin /usr/local
-
 # install homebrew
-ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
 # install homebrew packages
 brew tap homebrew/bundle
