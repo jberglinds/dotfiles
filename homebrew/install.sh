31a51d5a09fa7c4c62adda9cc7deeca636aebf10
Jonathan Berglind <jonathan.berglind@kry.se>
2021-12-08 15:48:18 +0100

    Update homebrew conf

--- homebrew/install.sh
+++ homebrew/install.sh
@@ -3,6 +3,10 @@
 # install homebrew
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
+brew() {
+  /opt/homebrew/bin/brew "$@"
+}
+
 # install homebrew packages
 brew tap homebrew/bundle
 brew bundle
